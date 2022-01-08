8e9082ae53957b6d2232853b26d83620e37d0063
Yaman-Habip <56274952+Yaman-Habip@users.noreply.github.com>
2022-01-08 18:20:07 -0500

    Update finalTwillio.py

--- finalTwillio.py
+++ finalTwillio.py
@@ -8,10 +8,6 @@ import requests
 
 #Admin phone is the sending phone from twillio
 AdminPhoneNumber="+14153012091"
-#Twillio Account ID
-account_sid = 'AC5a0bfbd5b477c769757be6b337e1160b'
-#Twillio API token
-auth_token = '6675a2a7f78498e13fb78e0394a58b7e'
 
 def send_message(msg,phone):
 	message = msg
