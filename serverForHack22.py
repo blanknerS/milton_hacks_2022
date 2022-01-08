f4396489485be96e0ee526c22911872965dc07fb
Andrew Rodriguez <andrewbrodriguez@gmail.com>
2022-01-08 17:28:37 -0500

    close to final

--- serverForHack22.py
+++ serverForHack22.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import parseSchedule
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/getInfo' )
 def collect_info():
